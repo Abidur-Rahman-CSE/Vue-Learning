18da97c84bc2105a9db533c8245013a2f621fefc
Abidur-Rahman-CSE <arahman171275@bscse.uiu.ac.bd>
2022-09-13 19:03:11 +0600

    data, methods, v-model etc

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,8 +1,38 @@
 <template>
     <h2 class="heading">Contact US</h2>
+    <div>Counter: {{ count }}</div>
+    <br>
+    <button @click="increament">( + )</button>
+    &nbsp;
+    &nbsp;
+    <button @click="dicreament">( - )</button>
+    <br>
+    <br>
+    <input type="number" v-model="count" />
     <section id="service2">
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, libero, fugit deserunt odit pariatur unde voluptas laboriosam porro ratione eum quidem aut esse dolorem facere. Magni dolor dolorum esse mollitia?</div>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, accusantium! Quibusdam harum corporis error officia autem praesentium iste laboriosam aperiam consequuntur velit id consequatur dolorum atque, odit cupiditate ex sapiente!</div>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, libero, fugit deserunt odit pariatur unde voluptas laboriosam porro ratione eum quidem aut esse dolorem facere. Magni dolor dolorum esse mollitia?</div>
     </section>
 </template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increament() {
+      this.count++;
+    },
+    dicreament() {
+      this.count--;
+    }
+  }
+}
+</script>
